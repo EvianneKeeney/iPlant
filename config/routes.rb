ca60d070f1172c4bf47e4a94a83c2f3d0c33ea65
Evianne Keeney <evianneelise@gmail.com>
2019-08-01 19:43:50 -0400

    Added devise and bootstrap to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,37 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :users, path: 'auth', path_names: { 
+    sign_in: 'sign_in', sign_out: 'sign_out',
+    password: 'secret', confirmation: 'verification', 
+    unlock: 'unblock', registration: 'register', sign_up: 'sign_up' 
+  }
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    devise_scope :user do
+      authenticated :user do
+        resources :plants
+      end
+    end
+
+    devise_scope :user do
+      authenticated :user do
+        namespace :api do
+          namespace :v1 do
+            resources :sessions, defaults: {format: :json}
+          end
+        end
+      end
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   resources :plants
 
